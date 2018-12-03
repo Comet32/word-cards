13d8e8e1df090acd8df6686f135363a2ccd7724f
zhaoenxiao <zhaoenxiao32@gmail.com>
2018-12-03 21:58:02 +0800

    在 QuizScreen 中添加了 ReTest 与 Return 按钮

--- components/QuizScreen.js
+++ components/QuizScreen.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Animated
+} from 'react-native'
 import { green, red, white, black } from '../utils/colors'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { NavigationActions } from 'react-navigation'
 
 export default class QuizScreen extends Component {
   state = {
     currentQuestion: 0,
     correctQuestion: 0,
     isAnswer: false,
-    isEnd: false
+    isEnd: false,
+    bounceValue: new Animated.Value(1)
   }
 
   handleAnswerSwitch = () => {
@@ -22,7 +31,7 @@ export default class QuizScreen extends Component {
       currentQuestion: state.currentQuestion + 1,
       correctQuestion: state.correctQuestion + 1
     }))
-    
+
     this.judgeEqual()
   }
 
@@ -31,7 +40,7 @@ export default class QuizScreen extends Component {
       isAnswer: false,
       currentQuestion: state.currentQuestion + 1
     }))
-    
+
     this.judgeEqual()
   }
 
@@ -45,19 +54,70 @@ export default class QuizScreen extends Component {
     }
   }
 
+  handleReturn = () => {
+    this.props.navigation.dispatch(
+      NavigationActions.navigate({ routeName: 'CardScreen' })
+    )
+  }
+
+  handleReTest = () => {
+    this.setState(() => ({
+      currentQuestion: 0,
+      correctQuestion: 0,
+      isAnswer: false,
+      isEnd: false,
+      bounceValue: new Animated.Value(1)
+    }))
+  }
+
   render() {
-    const { currentQuestion, correctQuestion, isAnswer,isEnd } = this.state
+    const {
+      currentQuestion,
+      correctQuestion,
+      isAnswer,
+      isEnd,
+      bounceValue
+    } = this.state
     const { questions } = this.props.navigation.state.params
 
-    if(isEnd){
+    if (isEnd) {
       successRate = Math.round((correctQuestion / questions.length) * 100)
 
-      return(
-        <View style={[styles.container,{justifyContent: 'center'}]}>
+      Animated.sequence([
+        Animated.timing(bounceValue, { duration: 200, toValue: 1.04 }),
+        Animated.spring(bounceValue, { toValue: 1, friction: 4 })
+      ]).start()
+
+      return (
+        <Animated.View
+          style={[
+            styles.container,
+            { justifyContent: 'center', transform: [{ scale: bounceValue }] }
+          ]}
+        >
+          <MaterialCommunityIcons
+            name={'emoticon-happy'}
+            size={80}
+            color={green}
+          />
           <Text style={styles.endText}>Total Cards： {questions.length}</Text>
-          <Text style={styles.endText}>Your Correct Cards： {correctQuestion}</Text>
+          <Text style={styles.endText}>
+            Your Correct Cards： {correctQuestion}
+          </Text>
           <Text style={styles.endText}>Correct Rate：{successRate + '%'}</Text>
-        </View>
+          <TouchableOpacity
+            onPress={this.handleReturn}
+            style={[styles.Btn, { backgroundColor: black, marginTop: 50 }]}
+          >
+            <Text style={{ color: white, fontSize: 18 }}>Return to Deck</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={this.handleReTest}
+            style={[styles.Btn, { backgroundColor: black, marginTop: 20 }]}
+          >
+            <Text style={{ color: white, fontSize: 18 }}>Retest</Text>
+          </TouchableOpacity>
+        </Animated.View>
       )
     }
 
@@ -125,6 +185,6 @@ const styles = StyleSheet.create({
     marginBottom: 30
   },
   endText: {
-    fontSize: 30,
+    fontSize: 30
   }
 })
